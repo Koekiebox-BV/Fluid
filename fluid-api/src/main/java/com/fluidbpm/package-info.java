9890b3a8c2fbd49d54f4f25b330cd7b98ba25150
koekiebox <jason@koekiebox.com>
2018-09-14 11:08:00 +0400

    Updated package info.

---
+++ fluid-api/src/main/java/com/fluidbpm/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Root package for Fluid-BPM.
+ * 
+ * @since 1.8
+ * @author jasonbruwer
+ * @version 1.8
+ */
+package com.fluidbpm;
---
+++ fluid-api/src/main/java/com/fluidbpm/program/api/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Root API classes used by Fluid-BPM custom programs.
+ * See {@code IActionBase}.
+ * 
+ * @see com.fluidbpm.program.api.ICustomWebAction
+ * @see com.fluidbpm.program.api.ICustomProgram
+ * @see com.fluidbpm.program.api.ICustomScheduledAction
+ *
+ * @since 1.8
+ * @author jasonbruwer
+ * @version 1.8
+ */
+package com.fluidbpm.program.api;
