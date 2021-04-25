c3d2b7634ae7e629a9e599f2339479d0abcca775
koekiebox <jason@koekiebox.com>
2021-04-25 15:53:52 +0200

    Add additional package info.

---
+++ fluid-api/src/main/java/com/fluidbpm/program/api/util/command/impl/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Implementation classes for all commands supported in Fluid-BPM.
+ *
+ * @since 1.8
+ * @author jasonbruwer
+ * @version 1.8
+ */
+package com.fluidbpm.program.api.util.command.impl;
