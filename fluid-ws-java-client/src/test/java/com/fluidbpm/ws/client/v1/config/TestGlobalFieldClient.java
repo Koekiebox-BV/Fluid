61efd07915bcb84816ff6de2cefe544f130e638f
koekiebox <jason@koekiebox.com>
2022-05-26 14:34:06 +0200

    Add test case for global fields.

---
+++ fluid-ws-java-client/src/test/java/com/fluidbpm/ws/client/v1/config/TestGlobalFieldClient.java
@@ -0,0 +1,74 @@
+/*
+ * Koekiebox CONFIDENTIAL
+ *
+ * [2012] - [2017] Koekiebox (Pty) Ltd
+ * All Rights Reserved.
+ *
+ * NOTICE: All information contained herein is, and remains the property
+ * of Koekiebox and its suppliers, if any. The intellectual and
+ * technical concepts contained herein are proprietary to Koekiebox
+ * and its suppliers and may be covered by South African and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material is strictly
+ * forbidden unless prior written permission is obtained from Koekiebox.
+ */
+
+package com.fluidbpm.ws.client.v1.config;
+
+import com.fluidbpm.program.api.vo.field.Field;
+import com.fluidbpm.program.api.vo.ws.auth.AppRequestToken;
+import com.fluidbpm.ws.client.v1.ABaseClientWS;
+import com.fluidbpm.ws.client.v1.ABaseTestCase;
+import com.fluidbpm.ws.client.v1.user.LoginClient;
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Created by jasonbruwer on 15/12/28.
+ */
+public class TestGlobalFieldClient extends ABaseTestCase {
+
+	private LoginClient loginClient;
+
+	/**
+	 *
+	 */
+	@Before
+	public void init() {
+		ABaseClientWS.IS_IN_JUNIT_TEST_MODE = true;
+		this.loginClient = new LoginClient(BASE_URL);
+	}
+
+	/**
+	 *
+	 */
+	@After
+	public void destroy()
+	{
+		this.loginClient.closeAndClean();
+	}
+
+	/**
+	 *
+	 */
+	@Test
+	public void testFetchAllGlobalFields() {
+		if (!this.isConnectionValid()) return;
+
+		AppRequestToken appRequestToken = this.loginClient.login(USERNAME, PASSWORD);
+		TestCase.assertNotNull(appRequestToken);
+
+		String serviceTicket = appRequestToken.getServiceTicket();
+
+		GlobalFieldClient globalFieldClient = new GlobalFieldClient(BASE_URL,serviceTicket);
+		List<Field> allGlobals = globalFieldClient.getAllGlobalFields();
+		
+		TestCase.assertNotNull("Fields need to be set.", allGlobals);
+	}
+
+
+}
