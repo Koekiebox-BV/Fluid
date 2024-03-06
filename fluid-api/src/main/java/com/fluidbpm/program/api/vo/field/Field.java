45dd4668249e2c5a8643eb91d7446a4e5ad66faa
koekiebox <jason@koekiebox.com>
2024-03-06 14:23:34 +0100

    Fix multi choice allowed options.

--- fluid-api/src/main/java/com/fluidbpm/program/api/vo/field/Field.java
+++ fluid-api/src/main/java/com/fluidbpm/program/api/vo/field/Field.java
@@ -454,6 +454,8 @@ public class Field extends ABaseFluidElasticSearchJSONObject {
 					String stringVal = this.jsonObject.getString(JSONMapping.FIELD_VALUE);
 					if (stringVal != null && stringVal.startsWith("{")) {
 						this.setFieldValue(new MultiChoice(new JSONObject(stringVal)));
+					} else if (stringVal != null) {
+						this.setFieldValue(new MultiChoice(stringVal));
 					} else {
 						throw new IllegalArgumentException(String.format(
 								"Multi-Choice data type received value '%s' of type '%s'. Not allowed.",
